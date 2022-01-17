907f39036e0d096ad1a31b8dcdf92dadab9e8c09
Shuber <shuberinu@gmail.com>
2022-01-17 13:56:30 +0700

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -21,3 +21,22 @@ $(function () {
 	});
 	$(".open-menu").height($(window).height());
 });
+
+// Smooth Scrolling
+$(".nav-link").on("click", function (e) {
+	if (this.hash !== "") {
+		e.preventDefault();
+
+		const hash = this.hash;
+
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top,
+			},
+			800,
+			function () {
+				window.location.hash = hash;
+			}
+		);
+	}
+});
